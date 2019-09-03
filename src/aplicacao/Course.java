6a1e990282e972710e611b2623fc9f7bcb1c9428
Taisso Reni <taissoreni@gmail.com>
2019-09-03 12:42:18 -0300

    Acresecentado mensagem ola mundo

--- src/aplicacao/Course.java
+++ src/aplicacao/Course.java
@@ -6,7 +6,7 @@ public class Course {
     public static void main(String[] args) {
         
         System.out.println("Bom dia!");
-        
+        System.out.println("Ola mundo!");
         
         
     }
