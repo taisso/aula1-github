04338e837aed22f982f99ac4f155f9f3ec9d39f9
Taisso Reni <taissoreni@gmail.com>
2019-09-03 12:19:25 -0300

    Acrescentada mensagem de bom dia

--- src/aplicacao/Course.java
+++ src/aplicacao/Course.java
@@ -1,11 +1,11 @@
-package course;
+package aplicacao;
 
 public class Course {
 
   
     public static void main(String[] args) {
         
-        
+        System.out.println("Bom dia!");
         
         
         
